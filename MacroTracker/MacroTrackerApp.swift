159505bbef2ded7b34313b01283cde4f5d335478
Gregory Paton <gregpaton08@gmail.com>
2026-01-25 20:04:31 -0500

    Initial Commit

---
+++ MacroTracker/MacroTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroTrackerApp.swift
+//  MacroTracker
+//
+//  Created by Gregory Paton on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MacroTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
